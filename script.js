edc78f0905bbe2b4fc5b19f7c039a9e349d23f04
Marina Andrade Catão <marinavdac@gmail.com>
2022-03-21 18:37:22 -0300

    fix: adiciona requisito 02 e 03 em javascript

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ body.appendChild(titleEl);
 const title = document.getElementsByTagName('h1')[0];
 title.setAttribute('id', 'title');
 title.innerText = 'Paleta de Cores';
+
+const section = document.createElement('section');
+body.appendChild(section);
+section.setAttribute('id', 'color-palette');
+
+const colors = ['black', 'blue', 'red', 'green'];
+for (let i = 0; i < colors.length; i += 1) {
+  const colorItem = colors[i];
+
+  const colorSelector = document.createElement('div');
+  colorSelector.classList = (`color ${colorItem}`);
+  colorSelector.style.backgroundColor = (`${colorItem}`);
+  section.appendChild(colorSelector);
+}
