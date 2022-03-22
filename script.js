d30676f37429e78b7c3ce25723d2c17d8246e62a
Marina Andrade Catão <marinavdac@gmail.com>
2022-03-22 16:09:30 -0300

    feat: adiciona resolucao do requisito 06

--- script.js
+++ script.js
@@ -19,6 +19,28 @@ for (let i = 0; i < colors.length; i += 1) {
   colorSelector.style.backgroundColor = `${colorItem}`;
   section.appendChild(colorSelector);
 }
+
+const black = document.querySelectorAll('.color')[0];
+black.classList += " " + "selected";
+
+const secondColor = document.querySelectorAll('.color')[1];
+const thirdColor = document.querySelectorAll('.color')[2];
+const fourthColor = document.querySelectorAll('.color')[3];
+
+function handleSelected(event) {
+  const selectedItem = document.querySelector('.selected');
+  selectedItem.classList.remove('selected');
+  event.target.classList.add('selected');
+  console.log(event.target);
+}
+
+black.addEventListener('click', handleSelected);
+secondColor.addEventListener('click', handleSelected);
+thirdColor.addEventListener('click', handleSelected);
+fourthColor.addEventListener('click', handleSelected);
+
+console.log(handleSelected);
+
 const middleSection = document.createElement('section');
 middleSection.setAttribute('id', 'buttons');
 body.appendChild(middleSection);
@@ -28,25 +50,23 @@ button.setAttribute('id', 'clear-board');
 middleSection.appendChild(button);
 button.innerText = 'Limpar';
 button.addEventListener('click', function handleClean() {
-  let pixelWhite = document.getElementsByClassName('pixel');
+  const pixelWhite = document.getElementsByClassName('pixel');
   for (let i = 0; i < pixelWhite.length; i += 1) {
     pixelWhite[i].style.backgroundColor = 'white';
+    console.log(pixelWhite[i].classList.value)
   }
 });
 
 const board = document.createElement('section');
 body.appendChild(board);
 board.setAttribute('id', 'pixel-board');
+let input = '10';
+const pixelNumbers = Math.pow(input, 2);
 
-const pixelNumbers = [1, 2, 3, 4, 5];
-for (let i = 0; i < pixelNumbers.length; i += 1) {
-  const pixWidth = document.createElement('div');
-  pixWidth.classList = 'pixel width';
-  board.appendChild(pixWidth);
-  for (let j = 1; j < pixelNumbers.length; j += 1) {
-    const pixHeight = document.createElement('div');
-    pixHeight.classList = 'pixel height';
-    board.appendChild(pixHeight);
-  }
-
+for (let i = 0; i < pixelNumbers; i += 1) {
+  const pixelCreation = document.createElement('div');
+  board.style.width = `${input * 40}px`;
+  pixelCreation.classList = 'pixel';
+  pixelCreation.style.backgroundColor = 'white';
+  board.appendChild(pixelCreation);
 }
